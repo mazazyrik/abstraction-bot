eaccab5b7ddea6c7fb77dd1e1752bbf0fb7f779a
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2024-08-28 20:19:14 +0300

    fixes

--- speech/chat.py
+++ speech/chat.py
@@ -1,44 +1,86 @@
 import logging
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from test import txt
-# Убедитесь, что у вас есть доступ к GPU, если он доступен
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Загрузка модели и токенизатора для русского языка
-model_name = "sberbank-ai/rugpt3large_based_on_gpt2"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+import aiohttp
+import asyncio
+import json
 
 
-def generate_text(prompt, max_length=2000):
-    inputs = tokenizer.encode(prompt, return_tensors='pt').to(device)
-    outputs = model.generate(inputs, max_length=max_length,
-                             num_return_sequences=1)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+async def send_request(session, prompt):
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Api-Key AQVN2CNjLpFAwtwCIGbnhuOOegcF9Ac557YZBSmR"
+    }
+
+    while True:
+        async with session.post(url, headers=headers, json=prompt) as response:
+            if response.status == 429:
+                logging.warning(
+                    'Received 429 Too Many Requests. Retrying after delay...')
+                await asyncio.sleep(5)  # Задержка перед повторной попыткой
+                continue
+
+            result = await response.text()
+            result_json = json.loads(result)
+
+            try:
+                res = result_json.get('result').get('alternatives')[
+                    0].get('message').get('text')
+                return res  # Возвращаем результат
+            except (AttributeError, IndexError):
+                return (
+                    'Нейросеть не смогла расшифровать этот текст. '
+                    'Попробуйте ещё раз.\n\n'
+                    'Если вы делали разбор файла, то проверьте не состоит ли '
+                    'он из картинок.\n\n'
+                    'Если вы делали разбор аудио, то проверьте не '
+                    'состоит ли он из тишины.'
+                )
 
 
-def add_prompt(text):
+async def add_prompt(text):
     logging.info('add_prompt started')
 
-    prompt = (
-        "Ты ассистент который помогает пользователям в написании конспектов.\n"
-        "Хорошо я поняла. Ожидаю вводный текст\n"
-    )
+    prompt = {
+        "modelUri": "gpt://b1g9b4dhssf7u0rot67t/yandexgpt-lite",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokens": 2000
+        },
+        "messages": [
+            {
+                "role": "system",
+                "text": ("Ты ассистент который помогает "
+                         "пользователям в написании конспектов.")
+            },
+            {
+                "role": "user",
+                "text": text
+            },
+            {
+                "role": "assistant",
+                "text": 'Хорошо я поняла. Ожидаю вводный текст'
+            },
+        ]
+    }
 
     text_len = len(text)
-    num_chunks = -(-text_len // 2000)  # Округление вверх
-    results = []
+    num_chunks = -(-text_len // 4096)
+    prompts = []
 
     for i in range(num_chunks):
-        chunk = text[i * 2000:(i + 1) * 2000]
-        full_prompt = prompt + chunk
-        result = generate_text(full_prompt)
-        results.append(result)
+        chunk = text[i * 4096:(i + 1) * 4096]
+        prompt_copy = prompt.copy() 
+        prompt_copy['messages'].append(
+            {
+                "role": "user",
+                "text": chunk
+            }
+        )
+        prompts.append(prompt_copy)
 
-    # Объединение результатов
-    final_result = "\n".join(results)
-    return final_result
+    async with aiohttp.ClientSession() as session:
+        tasks = [send_request(session, p) for p in prompts]
+        results = await asyncio.gather(*tasks)
 
-
-print(add_prompt(txt))
+    return results
